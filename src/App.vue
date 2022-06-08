77e55caf51366a1065b2d8bfc10113f767cfd298
ttania <ttania_m@hotmail.com>
2022-06-08 13:39:31 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<Panel>
+    <div class="grid">
+    <div class="col">Formulário</div>
+    
+    </div>
+
+  <div id="app">
+    <h1>Formulario</h1>
+
+    <span class="p-float-label">
+      <InputText id="username" type="text" v-model="value" />
+      <label for="username">Nome Completo</label>
+    </span>
+    
+    <span class="p-float-label">
+      <InputMask id="cpf" mask="999-999-999-99" v-model="val1" />
+      <label for="cpf">CPF</label>
+    </span>
+
+    <span class="p-float-label">
+    <InputMask id= "Telefone" mask="(999) 999-9999" v-model="val4" />
+    <label for="Phone">Telefone</label>
+    </span>
+    <!--
+    <div class="field col-12 md:col-4">
+      <label for="basic">CPF</label>
+      <InputMask mask="999-999-999-99" v-model="val1" placeholder="CPF" />
+    </div>
+    -->
+
+    <h5>Raised Buttons</h5>
+    <Button label="Primary" class="p-button-raised" />
+    <Button label="Secondary" class="p-button-raised p-button-secondary" />
+  </div>
+
+</Panel>
+
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'
+import '/node_modules/primeflex/primeflex.css'
+
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+import Card from 'primevue/card';
+import Panel from 'primevue/panel'
+import InputText from 'primevue/inputtext';
+import InputMask from 'primevue/inputmask';
+
+
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component('Dialog', Dialog);
+app.component('Button', Button);
+app.component('Card', Card);
+app.component('Panel', Panel);
+app.component('InputText' , InputText);
+app.component('InputMask', InputMask);
+
+app.mount('#app')
